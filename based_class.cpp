d9e4d4b587f68c8193d81cef068cc88bb9df5e37
Dima Pavlov <bigfatrussiancat@gmail.com>
2022-04-17 00:23:07 +0300

    работа над основным классом. Класс практически завершен. main.cpp - основной файл. Здесь задается сам класс и идет работа с пользователем. main.h - заголовочный файл основного класса. based_class.cpp - файл описания функций основного класса

---
+++ based_class.cpp
@@ -0,0 +1,51 @@
+#include"main.h"
+
+// number_x - номер коэффициента, который нужно изменить x - само число 
+void Main::Set(int number_x, double x)
+{
+    arr_degree_n[number_x] = x;
+};
+
+int Main::Get(int number_x)
+{
+    return arr_degree_n[number_x];
+};
+
+void Main::Show()
+{
+    int tmp = degree_n;
+    cout << "\nмногочлен: ";
+    for (int i = 0; i <= degree_n; i++)
+    {
+        if(!i)
+        {
+            cout << " " << arr_degree_n[i] << "*X" << tmp;
+            tmp--;
+            continue;
+        } 
+        if(arr_degree_n[i] > 0) cout << " + " << arr_degree_n[i] << "*X" << tmp;
+        else cout << " - " << abs(arr_degree_n[i]) << "*X" << tmp;
+        
+        tmp--;
+    }
+    cout << "\n";
+};
+
+
+void Main::Change(double open_arr_degree_n[])
+{
+    for (int i = 0; i <= degree_n; i++) arr_degree_n[i] = open_arr_degree_n[i];
+};
+
+void Main::Calculate_x(int number_x)
+{
+    double end_x = 0;
+    int tmp = degree_n;
+    for (int i = 0; i <= degree_n; i++)
+    {
+        end_x += arr_degree_n[i] * pow(number_x, tmp);
+        tmp--;
+    }
+    cout << "многочлен равен = " << end_x << "\n";
+    
+}
---
+++ main.h
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+class Main
+{
+protected:
+    int degree_n; //степень многочлена n
+    double *arr_degree_n = new double [degree_n + 1]; //все коэффициенты многочлена n-ой степени
+    
+
+
+public:
+    // конструктор по умолчанию
+    Main()
+    {
+        degree_n = 0;
+    };
+
+    // конструктор с параметрами
+    Main(int open_degree_n, double open_arr_degree_n[])
+    {
+        degree_n = open_degree_n;
+        for (int i = 0; i <= degree_n; i++) arr_degree_n[i] = open_arr_degree_n[i];
+    };
+    
+    //задать значение
+    void Set(int number_x, double x);
+    
+    //получить значение
+    int Get(int number_x);
+
+    //функция покажет многочлен
+    void Show();
+    
+    //функция для изменения коэффициентов
+    void Change(double open_arr_degree_n[]);
+
+    //вычислениe значения многочлена для заданного числа
+    void Calculate_x(int number_x);
+
+    //деструктор
+    ~Main()
+    {
+        delete [] arr_degree_n;
+    }
+    
+};
+
