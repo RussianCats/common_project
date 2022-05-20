21bc083dc8ba2c823c278ebb9b5f74a7085caef8
Dima Pavlov <bigfatrussiancat@gmail.com>
2022-05-20 12:54:02 +0300

    Дополнены комментарии к классам

--- second_class.h
+++ second_class.h
@@ -4,6 +4,13 @@
 #include<vector>
 using namespace std;
 
+// 3. Создать производный класс Двучлен. Определить в классе наследнике:
+// перегруженную функцию для изменения коэффициентов,
+// функцию вычисления куба двучлена,
+// функцию вычисления произведения двух двучленов,
+// функцию вычисления суммы двух двучленов
+// функцию вычисления разности двух двучленов
+// нахождение корней двучлена.
 
 class Second: public Main<double> {
     
@@ -14,8 +21,10 @@ class Second: public Main<double> {
             degree_n = open_degree_n;
             for (int i = 0; i <= degree_n; i++) arr_degree_n[i] = open_arr_degree_n[i];
         };
-
-    void kub(){
+        
+        
+    // Раскрывает куб двучлена
+    void kub(){                           
         //(a+b)^3=a^3+3*a^2*b+3*a*b^2+b^3
         //(a-b)^3=a^3-3*a^2*b+3*a*b^2-b^3
         
@@ -29,10 +38,12 @@ class Second: public Main<double> {
             cout << pow(arr_degree_n[1],3.0) << "*X0 - " << 3*pow(arr_degree_n[1],2.0)*abs(arr_degree_n[0]) << "*X0*X1 + " << 3*pow(arr_degree_n[0],2.0)*arr_degree_n[1] << "*X0*X2 - " << pow(abs(arr_degree_n[0]),3.0) << "*X3\n";
         }
         if (arr_degree_n[0] < 0 && arr_degree_n[1] < 0){
-            cout << "- " << abs(pow(arr_degree_n[0],3.0)) << "*X3 - " << abs(3*pow(arr_degree_n[0],2.0)*arr_degree_n[1]) << "*X2*X0 - " << abs(3*pow(arr_degree_n[1],2.0)*arr_degree_n[0]) << "*X1*X0 - " << abs(pow(arr_degree_n[1],3.0)) << "*X0\n";
+            cout << "-" << abs(pow(arr_degree_n[0],3.0)) << "*X3 - " << abs(3*pow(arr_degree_n[0],2.0)*arr_degree_n[1]) << "*X2*X0 - " << abs(3*pow(arr_degree_n[1],2.0)*arr_degree_n[0]) << "*X1*X0 - " << abs(pow(arr_degree_n[1],3.0)) << "*X0\n";
         }
     };
     
+    
+    // Выводит произведение двух двучленов
     void proizv(){
         cout << "Введите коэффициенты второго двучлена:\n";
         double *vtdvu = new double[2];
@@ -69,7 +80,9 @@ class Second: public Main<double> {
         delete [] vtdvu;
     };
     
-    void summa(){
+    
+    // Выводит сумму двучленов
+    void summa(){                      
         double *vtdvu = new double[2];
         cout << "Введите коэффициенты второго двучлена:\n";
         for (int i=0; i<2;i++){
@@ -91,6 +104,8 @@ class Second: public Main<double> {
         delete [] vtdvu;
     };
     
+    
+    // Выводит разность двучленов
     void raznost(){
         double *vtdvu = new double[2];
         cout << "Введите коэффициенты второго двучлена:\n";
@@ -113,11 +128,15 @@ class Second: public Main<double> {
         delete [] vtdvu;
     };
     
+    
+    // Функция нахождения корней двучлена 
     void korni(){
         cout << "Корень уравнения ";
         cout << "X = " << ((-1)*arr_degree_n[1])/(arr_degree_n[0]) << "\n";
     };
     
+    
+    // Перегруженная функция для изменения коэффициентов
     void Change(double open_arr_degree_n[])
     {
         for (int i = 0; i <= degree_n; i++) arr_degree_n[i] = open_arr_degree_n[i];
